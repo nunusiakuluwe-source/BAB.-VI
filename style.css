ee2927e6ecef426f49e69bb99e3dbb435b8e11e8
nunusiakuluwe-source <nunusiakuluwe@gmail.com>
2026-07-05 00:57:44 +0700

    Update style.css

--- style.css
+++ style.css
@@ -1,1 +1,164 @@
+*{
+    margin:0;
+    padding:0;
+    box-sizing:border-box;
+}
 
+html{
+    scroll-behavior:smooth;
+}
+
+body{
+
+    font-family:Poppins,sans-serif;
+
+    background:#F8F6F2;
+
+    color:#444;
+
+}
+
+.hero{
+
+    width:100%;
+    height:100vh;
+
+    display:flex;
+
+    justify-content:center;
+
+    align-items:center;
+
+    text-align:center;
+
+    padding:30px;
+
+    animation:fadeIn 2s;
+
+}
+
+.content{
+
+    max-width:700px;
+
+}
+
+.univ{
+
+    letter-spacing:5px;
+
+    font-size:13px;
+
+    color:#888;
+
+    margin-bottom:60px;
+
+}
+
+h1{
+
+    font-family:"Playfair Display",serif;
+
+    font-size:95px;
+
+    color:#3f3a34;
+
+}
+
+h2{
+
+    margin-top:20px;
+
+    font-weight:400;
+
+    line-height:1.8;
+
+}
+
+.desc{
+
+    margin-top:30px;
+
+    color:#777;
+
+    line-height:1.8;
+
+}
+
+.line{
+
+    width:120px;
+
+    height:2px;
+
+    background:#c8ab71;
+
+    margin:50px auto;
+
+}
+
+.for{
+
+    color:#999;
+
+    margin-bottom:10px;
+
+}
+
+h3{
+
+    font-family:"Playfair Display",serif;
+
+    font-size:36px;
+
+    margin-bottom:45px;
+
+}
+
+button{
+
+    background:#C6A66B;
+
+    color:white;
+
+    border:none;
+
+    border-radius:50px;
+
+    padding:18px 45px;
+
+    cursor:pointer;
+
+    font-size:17px;
+
+    transition:.35s;
+
+}
+
+button:hover{
+
+    transform:translateY(-4px);
+
+    background:#b18e4e;
+
+}
+
+@keyframes fadeIn{
+
+from{
+
+opacity:0;
+
+transform:translateY(40px);
+
+}
+
+to{
+
+opacity:1;
+
+transform:translateY(0);
+
+}
+
+}
