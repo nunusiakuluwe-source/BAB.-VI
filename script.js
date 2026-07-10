edd9eb588f6f25f06e78c320a01b6b2de0313432
nunusiakuluwe-source <nunusiakuluwe@gmail.com>
2026-07-11 06:52:13 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const tombol = document.getElementById("startButton");
-
-tombol.addEventListener("click",()=>{
-
-    alert("Selamat datang di BAB VI.\n\nMulai dari sini, kita akan menuliskan bab baru kehidupan.");
-
-});
